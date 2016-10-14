55955c93c56de395593282433174c3ecad9b9d3d
KATO Tomoyuki <kato.tomoyuki@jp.fujitsu.com>
2016-10-14 13:48:21 +0900

    Add plug-in summary for osc doc
    Stevedore Sphinx extension handles this comment.
    http://docs.openstack.org/developer/python-openstackclient/plugin-commands.html
    
    Change-Id: I3b2534b3ff2859f1528c8fc8145c9285e2a39907

--- congressclient/osc/osc_plugin.py
+++ congressclient/osc/osc_plugin.py
@@ -12,6 +12,8 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
+"""OpenStackClient plugin for Governance service."""
+
 import logging
 
 from congressclient.common import utils
